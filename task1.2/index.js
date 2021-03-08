750159b6b8f95929bd9013365a345ac53daabc0e
505019759 <505019759@qq.com>
2021-03-08 17:31:24 +0800

    Home task 1

---
+++ task1.2/index.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import readline from 'readline';
+import csv from 'csvtojson';
+
+const csvFileReadStream = fs.createReadStream('./task1.2/csv/books.csv');
+const jsonFileWriteStream = fs.createWriteStream('./task1.2/books.txt', { flags: 'w' })
+const rl = readline.createInterface(csvFileReadStream);
+let firstLine = null
+rl.on('line', function(line) {
+	if (!firstLine) {
+		firstLine = line;
+	} else {
+		const csvLineToParse = `${firstLine}\n${line}`;
+		csv()
+		.fromString(csvLineToParse)
+		.then((parsed) => {
+			const newJsonLine = parsed[0];
+			jsonFileWriteStream.write(`${JSON.stringify(newJsonLine)} \n`, logError);
+		})
+	}
+});
+
+const logError = (error) => {
+	if(error) {
+		console.log(error);
+	}
+};
+
+csvFileReadStream.on('error', logError)
+
+jsonFileWriteStream.on('error', logError)
